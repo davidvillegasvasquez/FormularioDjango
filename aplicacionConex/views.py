0dbba577189edc61a4d09fa4be2799170405fa15
davidvillegasvásquez <davidvillegas1@hotmail.com>
2021-11-01 14:41:20 -0400

    011121

--- aplicacionConex/views.py
+++ aplicacionConex/views.py
@@ -30,8 +30,25 @@ def ConsulTicketCarton(request):
 def ConsulTicketCarton(request):  
     
     formulario = FormNroCarton(request.GET)
-    monto = formulario['campoIdTicket'].value()
-    if monto == 'papa':
-        monto='monto es papa'
+    idTicket = formulario['campoIdTicket'].value()
+    if idTicket is not None: #Primero evaluamos que haya algo en el control.
+        try: #Luego evaluamos que haya conexión.
+            conn = psycopg2.connect(host="localhost", database="controltickets", user="david", password="1234")
+
+        except (Exception, psycopg2.DatabaseError) as error:
+            monto = "sin servicio, intente de nuevo o más tarde"
+
+        else:
+            cursor = conn.cursor()
+            cursor.execute("select monto from ticket_cartón_recibidos where id_cartón = %s",(idTicket,))
+            monto = cursor.fetchone()
+            if monto is not None:
+                monto = monto[0]
+            else: 
+                monto = ""
+            conn.close()
+    else:
+        monto = ""
+
     context = {'widget_formulario' : formulario, 'montoPlantilla': monto }
     return render(request, "consultaCarton.html", context)
