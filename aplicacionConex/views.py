46ac3241c0a94262457c7bd3c2506f2e5c758d26
davidvillegasvásquez <davidvillegas1@hotmail.com>
2021-11-02 18:38:08 -0400

    021121

--- aplicacionConex/views.py
+++ aplicacionConex/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 import psycopg2
 from aplicacionConex.forms import FormNroCarton
 from django.http import HttpResponseRedirect, HttpResponse
+from config import config
 # Create your views here.
 
 def index(request):
-    #codigos="92345a"
-   
+    paramConex = config()
     try:
-        conn = psycopg2.connect(host="localhost", database="controltickets", user="david", password="1234")
+        conn = psycopg2.connect(**paramConex)
 
     except (Exception, psycopg2.DatabaseError) as error:
         idDelCliente1="sin servicio, intente de nuevo o más tarde"
@@ -20,27 +20,22 @@ def index(request):
         conn.close()
         
     return render(request, "index.html", context={"IdClienteConsig1" : idDelCliente1})
-    #Coloqué un literal -"92345a"- directamente en el diccionario context.
 
-"""
-def ConsulTicketCarton(request):
-    return render(request, "consultaCarton.html", context={"montoEnPlantilla" : "8000Bs"})
-
-"""
 def ConsulTicketCarton(request):  
     
     formulario = FormNroCarton(request.GET)
     idTicket = formulario['campoIdTicket'].value()
     if idTicket is not None: #Primero evaluamos que haya algo en el control.
         try: #Luego evaluamos que haya conexión.
-            conn = psycopg2.connect(host="localhost", database="controltickets", user="david", password="1234")
+            paramConex = config()
+            conn = psycopg2.connect(**paramConex)
 
         except (Exception, psycopg2.DatabaseError) as error:
             monto = "sin servicio, intente de nuevo o más tarde"
 
         else:
             cursor = conn.cursor()
-            cursor.execute("select monto from ticket_cartón_recibidos where id_cartón = %s",(idTicket,))
+            cursor.execute("select monto from ticket_cartón_recibidos where id_cartón = %s limit 1",(idTicket,))
             monto = cursor.fetchone()
             if monto is not None:
                 monto = monto[0]
