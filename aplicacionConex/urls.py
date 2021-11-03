c45e889fe8352653c149922ca06721f58289ffd6
davidvillegasvásquez <davidvillegas1@hotmail.com>
2021-11-03 17:29:01 -0400

    031121modelo1

--- aplicacionConex/urls.py
+++ aplicacionConex/urls.py
@@ -2,4 +2,4 @@ from django.urls import path
 from . import views
 from django.conf.urls import url
 
-urlpatterns = [url(r'^$', views.index, name='index'), path('busquedaticket', views.ConsulTicketCarton, name='plantConsulTickCart'),]
+urlpatterns = [url(r'^$', views.index, name='index'), ]#path('busquedaticket', views.ConsulTicketCarton, name='plantConsulTickCart'),]
